36f4c724855fca93f2cc2f2ceb42eba9ff70f8ce
William Morriss <wjmelements@gmail.com>
2018-12-21 13:18:26 -0800

    update controller address

--- bin/js/fetchMintEvents.js
+++ bin/js/fetchMintEvents.js
@@ -8,7 +8,7 @@ const TrueUSDAddress = '0x8dd5fbCe2F6a956C3022bA3663759011Dd51e73E';
 const TrueUSDAbi = require('../abi/trueUsdAbi.json').abi;
 const TrueUSD = new web3.eth.Contract(TrueUSDAbi, TrueUSDAddress);
 
-const ControllerAddress = '0x000000c96e715A5B8cD3Beaea66bdb749225fA2F';
+const ControllerAddress = '0x0000000000075EfBeE23fe2de1bd0b7690883cc9';
 const ControllerAbi = require('../abi/controllerAbi.json').abi;
 const Controller = new web3.eth.Contract(ControllerAbi, ControllerAddress);
 
