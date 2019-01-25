a25b80784854b586115976f8b12f1416a3086ea1
William Morriss <wjmelements@gmail.com>
2019-01-25 15:38:38 -0800

    read custom ethrpc

--- bin/js/web3.js
+++ bin/js/web3.js
@@ -2,8 +2,18 @@
 // Include it with:
 // web3 = require('./web3.js');
 
+fs = require('fs');
+
 const Web3 = require('web3');
-const providerUrl = 'https://mainnet.infura.io';
+const defaultProviderUrl = 'https://mainnet.infura.io';
+
+let customProviderUrl;
+try {
+  customProviderUrl = fs.readFileSync('./config/ethrpc', 'utf8');
+} catch (e) {}
+
+const providerUrl = customProviderUrl || defaultProviderUrl;
+
 const web3 = new Web3(new Web3.providers.HttpProvider(providerUrl));
 
 module.exports = web3
